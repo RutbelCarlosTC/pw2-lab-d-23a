a09a9723a6ca6146cc4d8bcec058abd53f761c14
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-05-14 20:23:01 -0500

    se desordeno el array de numeros

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function generarTeclado() {
     // Array de números del 0 al 9
     var numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    // Desordenar el array de números aleatoriamente
+    numeros.sort(function() { return 0.5 - Math.random() });
 }
