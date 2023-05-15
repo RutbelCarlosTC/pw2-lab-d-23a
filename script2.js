e459ad37d274b5f968e1a3c26bbb8063f4e71168
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-05-14 20:39:45 -0500

    se agrego la pantalla, objeto memoria y la pila

--- script2.js
+++ script2.js
@@ -1,33 +1,6 @@
-// Pila para almacenar los datos
-const stack = [];
-// Función para manejar el evento de tecla presionada
-function handleKeyDown(event) {
-    if (event.key === "Enter") {
-      event.preventDefault(); // Evita que el formulario se envíe
-  
-      const display = document.getElementById("display");
-      const input = display.value.trim();
-  
-      if (input !== "") {
-        addToStack(input);
-        display.value = "";
-      }
-    }
-}
-// Función para agregar un elemento a la pila
-function addToStack(item) {
-    stack.push(item);
-    updateStackDisplay();
-}
-// Función para actualizar la visualización de la pila
-function updateStackDisplay() {
-    const stackElement = document.getElementById("stack");
-    stackElement.innerHTML = ""; // Limpiar la visualización actual
-  
-    // Crear elementos de lista para cada elemento de la pila
-    stack.forEach((item) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = item;
-      stackElement.appendChild(listItem);
-    });
-}
+const screen = document.querySelector('.screen');
+const memory = {
+    m1: null,
+    m2: null
+};
+let stack = [];
