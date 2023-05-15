76f787ee4605fe60977345bee4feb88bafa82746
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-05-14 20:24:02 -0500

    se crearon los botones con numeros aleatorios

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ function generarTeclado() {
     var numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     // Desordenar el array de números aleatoriamente
     numeros.sort(function() { return 0.5 - Math.random() });
+    // Crear los botones del teclado con los números aleatorios
+    numeros.forEach(function(numero) {
+        var boton = document.createElement('button');
+        boton.textContent = numero;
+        boton.addEventListener('click', function() {
+          generarClave(numero);
+        });
+        document.getElementById('teclado').appendChild(boton);
+    });
 }
