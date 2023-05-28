f17308a59a66ed7f52634d06715bd6b3c5233979
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-05-28 17:08:19 -0500

    se modifico la funcion mostrarLista() para recibir un JSON con la estructura de los eventos y mostrarlos en formato lista de html

--- Lab3/public/script.js
+++ Lab3/public/script.js
@@ -1,13 +1,39 @@
 function mostrarLista() {
+  fetch('http://localhost:3000/list')
+  .then(response => response.json())
+  .then(events => {
+    console.log(events);
+    let html = '<h1>Agenda Personal</h1>\n';
+    html += '<h2>Ver eventos</h2>\n';
 
-    fetch('http://localhost:3000/list')
-    .then(response => response.text())
-    .then(html => {
-        console.log(html);
-      const mainDiv = document.getElementById('main');
-      mainDiv.innerHTML = html;
-    })
-    .catch(error => {
-      console.error('Error al obtener el HTML:', error);
-    });
+    html += '<ul>\n';
+
+    // Itera sobre las fechas y eventos del JSON
+    for (const date in events) {
+      if (events.hasOwnProperty(date)) {
+        const eventsForDate = events[date];
+        html += `<li>${date}<ul>`;
+        // Itera sobre los eventos de la fecha y crea elementos de lista para cada uno
+        eventsForDate.forEach(event => {
+          html += `
+        <li>
+          <a href="#" onclick="verEvento('${event.date}','${event.time}')">${event.time}->[${event.title}]</a>
+          <a href="#" onclick="editarEvento('${event.date}','${event.time}')">Editar</a>   
+          <a href="#" onclick="borrarEvento('${event.date}','${event.time}')">Eliminar</a>
+        </li>
+        `;
+
+        });
+        html += '</ul></li>';
+      }
+    }      
+    html += '</ul>';
+    html +='<br>';
+    html += '<button type="button" onclick="formCrearEvento()">CREAR NUEVO EVENTO</button>';
+    const mainDiv = document.getElementById('main');
+    mainDiv.innerHTML = html;
+  })
+  .catch(error => {
+    console.error('Error al obtener el HTML:', error);
+  });
 }
