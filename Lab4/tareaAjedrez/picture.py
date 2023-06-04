4941f58c1d35b0f2884891130c318a46f733d74a
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-03 19:01:31 -0500

    se completo funcion rotate

--- Lab4/tareaAjedrez/picture.py
+++ Lab4/tareaAjedrez/picture.py
@@ -106,4 +106,12 @@ class Picture:
   def rotate(self):
     """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
     o antihorario"""
-    return Picture(None)
+    #antihorario:
+    rotado = []
+    for j in range(len(self.img[0]) - 1, -1, -1):
+        aux = ""
+        for i in range(len(self.img)):
+            aux += self.img[i][j]
+        rotado.append(aux)
+
+    return Picture(rotado)
