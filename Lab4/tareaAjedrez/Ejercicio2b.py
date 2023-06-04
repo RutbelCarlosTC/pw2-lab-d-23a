1afaf3c3232a6330f91a95ddc209cfd6be279adb
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-03 19:03:00 -0500

    se completo ejercicio b

--- Lab4/tareaAjedrez/Ejercicio2b.py
+++ Lab4/tareaAjedrez/Ejercicio2b.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+CaballosA = knight.join(knight.negative())
+CaballosB = CaballosA.verticalMirror()
+
+grupoCaballos = Picture(CaballosA.img + CaballosB.img)
+draw(grupoCaballos)
