64d6536f3734c759bc497d318abd7879662f0d9e
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-03 19:06:26 -0500

    se creo fila de cuadros, piezas y peones

--- Lab4/tareaAjedrez/Ejercicio2g.py
+++ Lab4/tareaAjedrez/Ejercicio2g.py
@@ -1,2 +1,17 @@
 from interpreter import draw
 from chessPictures import *
+
+izq = (rock.join(knight)).join(bishop)
+centro = queen.join(king)
+der = (bishop.join(knight)).join(rock)
+
+filaCuadrosA = (square.join(square.negative())).horizontalRepeat(4)
+filaCuadrosB = filaCuadrosA.negative()
+
+peones = pawn.horizontalRepeat(8)
+piezas = (izq.join(centro)).join(der)
+
+blancas  = Picture(peones.img + piezas.img)
+negras = Picture(piezas.img + peones.img).negative()
+
+filaCuadrosAB = Picture(filaCuadrosA.img + filaCuadrosB.img)
