087455eb3e36f8703c6917914aff533a6e2e83ff
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-03 19:05:16 -0500

    se completo ejercicio f grupo de cuadros

--- Lab4/tareaAjedrez/Ejercicio2f.py
+++ Lab4/tareaAjedrez/Ejercicio2f.py
@@ -1,2 +1,8 @@
 from interpreter import draw
 from chessPictures import *
+
+filaCuadrosA = (square.join(square.negative())).horizontalRepeat(4)
+filaCuadrosB = filaCuadrosA.negative()
+filaCuadrosAB = Picture(filaCuadrosA.img + filaCuadrosB.img)
+grupoCuadros = filaCuadrosAB.verticalRepeat(2)
+draw(grupoCuadros)
