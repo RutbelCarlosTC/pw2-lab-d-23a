9a0fc89022f20aa9fbd06a7c4febb0706b793fe1
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-11 18:08:21 -0500

    se agrego conteo de numero de visitas a la vista index

--- lab5/ejercicioLibrary/src/locallibrary/catalog/views.py
+++ lab5/ejercicioLibrary/src/locallibrary/catalog/views.py
@@ -14,14 +14,18 @@ def index(request):
 
     # EL 'all()' esta implicito por defecto.
     #Author.objects.all().count()
-
     num_authors = Author.objects.count()
+
+    # Numero de visitas a esta vista, como countador en la  variable ssesion.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits+1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits':num_visits,
     }
 
     # Renderizar la platilla HTML index.html con los datos de la variable context
