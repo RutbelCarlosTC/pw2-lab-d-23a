c1e27308c9b26a19542fa299f56d328c56a4c670
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-11 10:40:56 -0500

    agregando los modelos Genero y lenguaje

--- lab5/ejercicioLibrary/src/locallibrary/catalog/models.py
+++ lab5/ejercicioLibrary/src/locallibrary/catalog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+
+class Genre(models.Model):
+    """Model que representa un genero de libro (e.g. Science Fiction, Non Fiction)."""
+    name = models.CharField(
+        max_length=200,
+        help_text="Ingresa un genero de libro (e.g. Science Fiction, French Poetry etc.)"
+        )
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Ingresa the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
