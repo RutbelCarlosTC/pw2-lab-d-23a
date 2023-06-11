164ad521e25edd1eac96864478c3a3fe9657350e
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-11 15:12:08 -0500

    Vista genérica basada en clases que enumera libros prestados al usuario actual

--- lab5/ejercicioLibrary/src/locallibrary/catalog/views.py
+++ lab5/ejercicioLibrary/src/locallibrary/catalog/views.py
@@ -26,3 +26,38 @@ def index(request):
 
     # Renderizar la platilla HTML index.html con los datos de la variable context
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    """Generic class-based view for a list of books."""
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    """Generic class-based detail view for a book."""
+    model = Book
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Vista genérica basada en clases que enumera libros prestados al usuario actual."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
