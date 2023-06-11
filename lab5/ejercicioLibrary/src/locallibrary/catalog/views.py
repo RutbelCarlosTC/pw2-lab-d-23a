169ad38b4014b4792abfa2dda6f1833a86bc4fd3
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-11 13:08:51 -0500

    Vista Funcion para pagina de inicio

--- lab5/ejercicioLibrary/src/locallibrary/catalog/views.py
+++ lab5/ejercicioLibrary/src/locallibrary/catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book,Author,BookInstance,Genre
+def index(request):
+    """Vista Funcion para pagina de inicio"""
+
+    # Numero de libros e instancias de libro
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Libros disponibles (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # EL 'all()' esta implicito por defecto.
+    #Author.objects.all().count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Renderizar la platilla HTML index.html con los datos de la variable context
+    return render(request, 'index.html', context=context)
