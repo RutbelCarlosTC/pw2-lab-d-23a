4a2f4223ba77cb64c833113c7b0b8ddbab7a955d
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2023-06-11 10:48:09 -0500

    agregnado modelo BookInstance que representa la copia de un libro

--- lab5/ejercicioLibrary/src/locallibrary/catalog/models.py
+++ lab5/ejercicioLibrary/src/locallibrary/catalog/models.py
@@ -56,3 +56,45 @@ class Book(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.title
+
+import uuid  # Required for unique book instances
+from datetime import date
+
+from django.contrib.auth.models import User  # Required to assign User as a borrower
+
+
+class BookInstance(models.Model):
+    """Modelo que representa una copia específica de un libro (se puede tomar prestado de la biblioteca)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determina si el libro está vencido según la fecha de vencimiento y la fecha actual."""
+        return bool(self.due_back and date.today() > self.due_back)
+
+    LOAN_STATUS = (
+        ('d', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='d',
+        help_text='Book availability')
+
+    class Meta:
+        ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return '{0} ({1})'.format(self.id, self.book.title)
